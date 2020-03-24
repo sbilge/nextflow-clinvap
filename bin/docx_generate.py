1af3f8cd53c65bf5d7d5703ee7826abaa1247b1b
Bilge Surun <bilges@Bilges-MacBook-Pro.local>
2020-03-25 00:02:02 +0100

    minor

--- bin/docx_generate.py
+++ bin/docx_generate.py
@@ -3,13 +3,6 @@ from docxtpl import DocxTemplate
 import sys
 import json
 
-
-if len(sys.argv) != 3:
-	print("usage: python {} ".format(
-		sys.argv[0]) + '<JSON report> <DOCX template> <Output file name>')
-	exit(1)
-
-
 json_report = sys.argv[1]
 word_template = sys.argv[2]
 output_file = sys.argv[3]
